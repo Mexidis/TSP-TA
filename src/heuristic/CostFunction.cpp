4100aca9521791f07bca021844bb15e145f17c81
Mexidis <mexidismartinez@gmail.com>
2025-09-08 01:01:14 -0600

    CostFunction.cpp implemented

--- src/heuristic/CostFunction.cpp
+++ src/heuristic/CostFunction.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "CostFunction.h"
+
+
+double CostFunction::normalizer( std::vector<City>& S,  CompleteGraph& GS) {
+        double maxDist = 0.0;
+        for (int i = 0; i < S.size(); ++i) {
+                for (int j = i+1; j < S.size(); ++j) {
+                        double d = GS.weigth(S[i].getId(), S[j].getId());
+                        maxDist = std::max(maxDist, d);
+                }
+        }
+        return maxDist * S.size(); // worst case estimate
+}
+
+double CostFunction::evaluate( Solution& sol, CompleteGraph& GS) {
+        return sol.length(GS);
+}
