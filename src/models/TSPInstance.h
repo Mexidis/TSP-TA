3f95c8cec11f3636e5f414ce9a557c83b19646e6
Mexidis <mexidismartinez@gmail.com>
2025-09-08 00:40:50 -0600

    TSPInstance.h definitions added

--- src/models/TSPInstance.h
+++ src/models/TSPInstance.h
@@ -5,6 +5,7 @@
 #ifndef TSP_TA_TSPINSTANCE_H
 #define TSP_TA_TSPINSTANCE_H
 #include "CompleteGraph.h"
+#include "Solution.h"
 
 
 class TSPInstance {
@@ -12,15 +13,16 @@ private:
         std::vector<City> S;      // instance subset cities
         const Graph& original_G;  // main graph with all cities
         CompleteGraph GS;         // complete graph made by S
-        double normalizer;        // N(S)
 
 public:
-        TSPInstance( std::vector<City>& subset, Graph& G);
+        TSPInstance(std::vector<City>& subset, Graph& G);
 
-        double getNormalizer() { return normalizer; }
-        CompleteGraph& getGS() { return GS; }
+        std::vector<Solution> generatePermutations();
+        bool isFeasable(Solution& sol);
+        double maxDistance();  // max distance in S
 
-        double computeNormalizer();   // calculates N(S)
+        GraphCompleto& getGS();
+
 };
 
 
