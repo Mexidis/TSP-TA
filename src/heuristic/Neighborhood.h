a602e356945158fc771d8dd6527768b67dc218f1
Mexidis <mexidismartinez@gmail.com>
2025-09-08 00:39:57 -0600

    Neighborhood.h definitions added

--- src/heuristic/Neighborhood.h
+++ src/heuristic/Neighborhood.h
@@ -5,8 +5,11 @@
 #ifndef TSP_TA_NEIGHBORHOOD_H
 #define TSP_TA_NEIGHBORHOOD_H
 
+#include "../models/Solution.h"
 
 class Neighborhood {
+        // generates a neighbor by exchanging two cities
+        static Solution generateNeighbor(const Solution& sol);
 };
 
 
