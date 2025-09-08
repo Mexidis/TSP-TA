ce369c48048f6407414ff74c10f34c889a1eeedc
Mexidis <mexidismartinez@gmail.com>
2025-09-08 00:40:34 -0600

    Solution.h definitions added

--- src/heuristic/ThresholdAccepting.h
+++ src/heuristic/ThresholdAccepting.h
@@ -4,9 +4,26 @@
 
 #ifndef TSP_TA_THRESHOLDACCEPTING_H
 #define TSP_TA_THRESHOLDACCEPTING_H
+#include <utility>
 
+#include "../models/Solution.h"
+#include "../models/CompleteGraph.h"
 
 class ThresholdAccepting {
+private:
+        double T;      // temperature
+        double phi;    // cooling factor
+        double epsilon;
+        int L;         // batch size
+        Solution bestSolution;
+
+public:
+        ThresholdAccepting(double initial_T, double phi, double epsilon, int L);
+
+        std::pair<double, Solution> calculaLote(double T, Solution& s, CompleteGraph& GS);
+        Solution execute(Solution& s_inicial, CompleteGraph& GS);
+
+        double initialTemperature(Solution& s, CompleteGraph& GS, double P);
 };
 
 
